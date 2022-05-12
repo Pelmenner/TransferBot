2c6dbbe1ac8757c024f82f9bd0d73e7b2050d9a0
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2022-05-13 00:28:35 +0300

    add database initialization

---
+++ db_init.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS Chats (
+    chat_id INTEGER NOT NULL,
+    chat_type TEXT NOT NULL,
+    token INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Subscriptions (
+    source_chat INTEGER NOT NULL,
+    destination_chat INTEGER NOT NULL,
+    FOREIGN KEY (source_chat) REFERENCES Chats(rowid),
+    FOREIGN KEY (destination_chat) REFERENCES Chats(rowid)
+);
+
+CREATE TABLE IF NOT EXISTS Messages (
+    destination_chat INTEGER NOT NULL,
+    sender TEXT,
+    message_text TEXT,
+    FOREIGN KEY (destination_chat) REFERENCES Chats(rowid)
+);
+
+CREATE TABLE IF NOT EXISTS Attachments (
+    data_type TEXT NOT NULL,
+    data_url TEXT NOT NULL,
+    parent_message INTEGER,
+    FOREIGN KEY (parent_message) REFERENCES Messages(rowid)
+);
