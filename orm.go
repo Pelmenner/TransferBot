63ba79a223a0117a9d592563df410146516460fe
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2022-06-02 22:43:05 +0300

    implement subscriptions

--- orm.go
+++ orm.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
@@ -26,6 +27,27 @@ type Chat struct {
 	Token string
 	Type  string
 	RowID int
+}
+
+func Transact(db *sql.DB, txOpts *sql.TxOptions, txFunc func(*sql.Tx) error) error {
+	tx, err := db.BeginTx(context.TODO(), txOpts)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+			panic(p)
+		} else if err != nil {
+			tx.Rollback()
+		} else {
+			err = tx.Commit()
+		}
+	}()
+
+	err = txFunc(tx)
+	return err
 }
 
 // findSubscribedChats returns all chats subscribed on the given one
@@ -70,8 +92,19 @@ func addChat(db *sql.DB, chatID int, chatType string) bool {
 }
 
 // getChat returns chat object with given id in messenger
-func getChat(db *sql.DB, chatID int, charType string) Chat {
-	return Chat{}
+// if an error occured, returns nil and logs error
+func getChat(db *sql.DB, chatID int, chatType string) *Chat {
+	res := Chat{ID: chatID, Type: chatType}
+	row := db.QueryRow(`SELECT token, rowid FROM Chats
+	WHERE chat_id = $1 AND chat_type = $2`, &chatID, &chatType)
+
+	err := row.Scan(&res.Token, &res.RowID)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+
+	return &res
 }
 
 // addUnsentMessage adds message to send later
@@ -84,16 +117,66 @@ func getUnsentMessages(db *sql.DB, maxCnt int) []Message {
 	return []Message{}
 }
 
+func getChatRowIDByToken(tx *sql.Tx, token string) (int, error) {
+	row := tx.QueryRow("SELECT rowid FROM Chats WHERE token = $1", &token)
+	rowID := -1
+	err := row.Scan(&rowID)
+	return rowID, err
+}
+
 // Subscribes proveded chat on another with given token.
 //  Returns true on success
 func subscribe(db *sql.DB, subscriber *Chat, subscriptionToken string) bool {
-	return false
+	err := Transact(db, &sql.TxOptions{
+		Isolation: sql.LevelSerializable,
+		ReadOnly:  false},
+		func(tx *sql.Tx) error {
+			subscriptionRowID, err := getChatRowIDByToken(tx, subscriptionToken)
+			if err != nil {
+				return err
+			}
+
+			if _, err := tx.Exec("INSERT INTO Subscriptions VALUES ($1, $2)",
+				&subscriptionRowID, &subscriber.RowID); err != nil {
+				return err
+			}
+
+			return nil
+		})
+
+	if err != nil {
+		log.Print(err)
+		return false
+	}
+
+	return true
 }
 
 // Unsubscribes provided chat from another with given token.
 //  Returns true on success
 func unsubscribe(db *sql.DB, subscriber *Chat, subscriptionToken string) bool {
-	return false
+	err := Transact(db, &sql.TxOptions{
+		Isolation: sql.LevelSerializable,
+		ReadOnly:  false},
+		func(tx *sql.Tx) error {
+			subscriptionRowID, err := getChatRowIDByToken(tx, subscriptionToken)
+			if err != nil {
+				return err
+			}
+			if _, err := tx.Exec("DELETE FROM Subscriptions WHERE source_chat = $1 AND destination_chat = $2",
+				&subscriptionRowID, &subscriber.RowID); err != nil {
+				return err
+			}
+
+			return nil
+		})
+
+	if err != nil {
+		log.Print(err)
+		return false
+	}
+
+	return true
 }
 
 // getUnusedAttachments returns all attachments which will be never sent anymore and deletes them
