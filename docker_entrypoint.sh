818f65eff5890909477305525453769224a56040
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2022-08-24 19:58:14 +0300

    update migrations

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 if ! [ -z $MIGRATE_DB ]; then
-    goose -dir ./migrations sqlite3 ./data/db.sqlite3 up
+    goose -dir ./migrations postgres "$DB_CONNECT_STRING" up
 fi
 
 /transfer_bot
--- migrations/002_alter_sender.sql
+++
@@ -1,8 +0,0 @@
--- +goose Up
-ALTER TABLE Messages
-ADD sender_chat TEXT NOT NULL
-DEFAULT "";
-
--- +goose Down
-ALTER TABLE Messages
-DROP COLUMN sender_chat IF EXISTS;
