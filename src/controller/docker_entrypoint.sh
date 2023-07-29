01f57bc6b4c2f8a8aff412a000a29ed327a1eda7
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2023-07-29 18:56:53 +0300

    refactoring

--- src/controller/docker_entrypoint.sh
+++ src/controller/docker_entrypoint.sh
@@ -7,12 +7,12 @@ until psql --dbname="$DB_CONNECT_STRING" -c '\q'; do
     sleep 3
 done
 
-if ! [ -z $MIGRATE_DB ]; then
+if [ -n "$MIGRATE_DB" ]; then
     goose -dir ./migrations postgres "$DB_CONNECT_STRING" up
 fi
 
-if ! [ -z "${DEBUG:-}" ];  then
-    dlv --listen=:$DEBUG_PORT --headless=true --api-version=2 --continue --accept-multiclient exec /transfer_bot
+if [ -n "${DEBUG:-}" ];  then
+    dlv --listen=:$DEBUG_PORT --headless=true --api-version=2 --accept-multiclient exec /transfer_bot
 else
     /transfer_bot
 fi
